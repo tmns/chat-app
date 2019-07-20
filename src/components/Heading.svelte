cb75f5590b607fd33b1b554e871142c90b6864fe
sairim <spiri@protonmail.com>
2019-07-20 13:25:23 +0300

    implement heading component

---
+++ src/components/Heading.svelte
@@ -0,0 +1,420 @@
+<style>
+  #heading {
+    font-family: "Bungee", cursive;
+    text-transform: uppercase;
+    font-size: 80px;
+    font-weight: bold;
+    margin: 0;
+    top: 0;
+    left: -6px;
+    color: blue;
+    mix-blend-mode: screen;
+    position: relative;
+    white-space: nowrap;
+    -webkit-filter: blur(1px);
+    -moz-filter: blur(1px);
+    filter: blur(1px);
+    -webkit-animation: skew 2s infinite, flash 0.04s infinite;
+    -moz-animation: skew 2s infinite, flash 0.04s infinite;
+    -o-animation: skew 2s infinite, flash 0.04s infinite;
+    animation: skew 2s infinite, flash 0.04s infinite;
+  }
+  #heading:after,
+  #heading:before {
+    content: attr(data-text);
+    position: absolute;
+    mix-blend-mode: screen;
+  }
+  #heading:after {
+    top: 0;
+    left: 6px;
+    color: blue;
+    -webkit-animation: move 1.5s infinite;
+    -moz-animation: move 1.5s infinite;
+    -o-animation: move 1.5s infinite;
+    animation: move 1.5s infinite;
+  }
+  #heading:before {
+    top: -2.4px;
+    left: 3.6px;
+    color: #b34e7e;
+  }
+  @-webkit-keyframes skew {
+    0% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    30% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    30.2% {
+      -webkit-transform: skewX(50deg);
+      -moz-transform: skewX(50deg);
+      -ms-transform: skewX(50deg);
+      -o-transform: skewX(50deg);
+      transform: skewX(50deg);
+    }
+    30.4% {
+      -webkit-transform: skewX(-50deg);
+      -moz-transform: skewX(-50deg);
+      -ms-transform: skewX(-50deg);
+      -o-transform: skewX(-50deg);
+      transform: skewX(-50deg);
+    }
+    31% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+  }
+  @-moz-keyframes skew {
+    0% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    30% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    30.2% {
+      -webkit-transform: skewX(50deg);
+      -moz-transform: skewX(50deg);
+      -ms-transform: skewX(50deg);
+      -o-transform: skewX(50deg);
+      transform: skewX(50deg);
+    }
+    30.4% {
+      -webkit-transform: skewX(-50deg);
+      -moz-transform: skewX(-50deg);
+      -ms-transform: skewX(-50deg);
+      -o-transform: skewX(-50deg);
+      transform: skewX(-50deg);
+    }
+    31% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+  }
+  @-o-keyframes skew {
+    0% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    30% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    30.2% {
+      -webkit-transform: skewX(50deg);
+      -moz-transform: skewX(50deg);
+      -ms-transform: skewX(50deg);
+      -o-transform: skewX(50deg);
+      transform: skewX(50deg);
+    }
+    30.4% {
+      -webkit-transform: skewX(-50deg);
+      -moz-transform: skewX(-50deg);
+      -ms-transform: skewX(-50deg);
+      -o-transform: skewX(-50deg);
+      transform: skewX(-50deg);
+    }
+    31% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+  }
+  @keyframes skew {
+    0% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    30% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    30.2% {
+      -webkit-transform: skewX(50deg);
+      -moz-transform: skewX(50deg);
+      -ms-transform: skewX(50deg);
+      -o-transform: skewX(50deg);
+      transform: skewX(50deg);
+    }
+    30.4% {
+      -webkit-transform: skewX(-50deg);
+      -moz-transform: skewX(-50deg);
+      -ms-transform: skewX(-50deg);
+      -o-transform: skewX(-50deg);
+      transform: skewX(-50deg);
+    }
+    31% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+  }
+  @-webkit-keyframes move {
+    0% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    30% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    31% {
+      -webkit-transform: translateX(-6px);
+      -moz-transform: translateX(-6px);
+      -ms-transform: translateX(-6px);
+      -o-transform: translateX(-6px);
+      transform: translateX(-6px);
+    }
+    33% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    98% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    100% {
+      -webkit-transform: translateX(6px);
+      -moz-transform: translateX(6px);
+      -ms-transform: translateX(6px);
+      -o-transform: translateX(6px);
+      transform: translateX(6px);
+    }
+  }
+  @-moz-keyframes move {
+    0% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    30% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    31% {
+      -webkit-transform: translateX(-6px);
+      -moz-transform: translateX(-6px);
+      -ms-transform: translateX(-6px);
+      -o-transform: translateX(-6px);
+      transform: translateX(-6px);
+    }
+    33% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    98% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    100% {
+      -webkit-transform: translateX(6px);
+      -moz-transform: translateX(6px);
+      -ms-transform: translateX(6px);
+      -o-transform: translateX(6px);
+      transform: translateX(6px);
+    }
+  }
+  @-o-keyframes move {
+    0% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    30% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    31% {
+      -webkit-transform: translateX(-6px);
+      -moz-transform: translateX(-6px);
+      -ms-transform: translateX(-6px);
+      -o-transform: translateX(-6px);
+      transform: translateX(-6px);
+    }
+    33% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    98% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    100% {
+      -webkit-transform: translateX(6px);
+      -moz-transform: translateX(6px);
+      -ms-transform: translateX(6px);
+      -o-transform: translateX(6px);
+      transform: translateX(6px);
+    }
+  }
+  @keyframes move {
+    0% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    30% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    31% {
+      -webkit-transform: translateX(-6px);
+      -moz-transform: translateX(-6px);
+      -ms-transform: translateX(-6px);
+      -o-transform: translateX(-6px);
+      transform: translateX(-6px);
+    }
+    33% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    98% {
+      -webkit-transform: none;
+      -moz-transform: none;
+      -ms-transform: none;
+      -o-transform: none;
+      transform: none;
+    }
+    100% {
+      -webkit-transform: translateX(6px);
+      -moz-transform: translateX(6px);
+      -ms-transform: translateX(6px);
+      -o-transform: translateX(6px);
+      transform: translateX(6px);
+    }
+  }
+  @-webkit-keyframes flash {
+    0% {
+      opacity: 1;
+      -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=100)";
+      filter: alpha(opacity=100);
+    }
+    50% {
+      opacity: 0.5;
+      -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=50)";
+      filter: alpha(opacity=50);
+    }
+  }
+  @-moz-keyframes flash {
+    0% {
+      opacity: 1;
+      -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=100)";
+      filter: alpha(opacity=100);
+    }
+    50% {
+      opacity: 0.5;
+      -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=50)";
+      filter: alpha(opacity=50);
+    }
+  }
+  @-o-keyframes flash {
+    0% {
+      opacity: 1;
+      -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=100)";
+      filter: alpha(opacity=100);
+    }
+    50% {
+      opacity: 0.5;
+      -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=50)";
+      filter: alpha(opacity=50);
+    }
+  }
+  @keyframes flash {
+    0% {
+      opacity: 1;
+      -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=100)";
+      filter: alpha(opacity=100);
+    }
+    50% {
+      opacity: 0.5;
+      -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=50)";
+      filter: alpha(opacity=50);
+    }
+  }
+</style>
+
+<script>
+  export let text;
+</script>
+
+<div id="heading">{text}</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,75 +1,81 @@
 <style>
-	* {
-		margin: 0;
-		padding: 0;
-		box-sizing: border-box;
-		overflow: hidden;
-	}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    overflow: hidden;
+  }
 
-	body {
-		font: 13px Helvetica, Arial;
-		background: #B34E7E;
-	}
+  body {
+    font: 13px Helvetica, Arial;
+    background: #b34e7e;
+  }
 
-	.interface {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-	}
+  .main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
 
-	form {
-		background: #01B3ED;
-		padding: 3px;
-		width: 100%;
-	}
+  form {
+    background: #01b3ed;
+    padding: 3px;
+    width: 100%;
+  }
 
-	form input {
-		border: 0;
-		padding: 10px;
-		width: 90%;
-		margin-right: .5%;
-		background: #B34E7E;
-		color: #FFF8B8;
-	}
+  form input {
+    border: 0;
+    padding: 10px;
+    width: 90%;
+    margin-right: 0.5%;
+    background: #b34e7e;
+    color: #fff8b8;
+  }
 
-	form button {
-		width: 9%;
-		background: #FFF8B8;
-		border: none;
-		padding: 10px;
-		color: #01B3ED;
-	}
+  form button {
+    background: #fff8b8;
+    border: none;
+    padding: 10px 13px;
+    color: #01b3ed;
+  }
 
-	#messages {
-		height: 600px;
-		width: 100%;
-		border: 3px solid #01B3ED;
-		list-style-type: none;
-		margin: 0;
-		padding: 0;
-	}
+  #messages {
+    height: 600px;
+    width: 100%;
+    border: 3px solid #01b3ed;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
 
-	#messages li {
-		padding: 5px 10px;
-	}
+  #messages li {
+    padding: 5px 10px;
+  }
 
-	#messages li:nth-child(odd) {
-		background: #01B3ED;
-	}
+  #messages li:nth-child(odd) {
+    background: #01b3ed;
+  }
 </style>
 
 <svelte:head>
-	<title>Chat App</title>
-	<script src="/socket.io/socket.io.js"></script>
+  <title>Chat App</title>
+  <script src="/socket.io/socket.io.js"></script>
+	<link href='https://fonts.googleapis.com/css?family=Bungee' rel='stylesheet' type='text/css'>
 </svelte:head>
 
+<script>
+	import Heading from '../components/Heading.svelte';
+</script>
+
 <body>
-	<div class="interface">
-		<ul id="messages"></ul>
-		<form action="">
-			<input id="m" autocomplete="off" /><button>Send</button>
-		</form>	
-	</div>
+  <div class="main">
+		<Heading text={'Chat App'} />
+    <ul id="messages" />
+    <form action="">
+      <input id="m" autocomplete="off" />
+      <button>Send</button>
+    </form>
+  </div>
 </body>
